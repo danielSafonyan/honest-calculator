e6a1ae7b53f82b1f507ba21e8d766fed44402f11
danielSafonyan <daniel.safonyan@gmail.com>
2022-06-05 20:18:01 +0100

    Stage 4/5

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -4,6 +4,10 @@ msg_2 = "Yes ... an interesting math operation. You've slept through all classes
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
 
 memory = 0.0
 
@@ -66,10 +70,24 @@ def is_one_digit(num):
     return False
 
 
+def lazy_check(x, operation, y):
+    msg = ''
+
+    if is_one_digit(x) and is_one_digit(y):
+        msg += msg_6
+    if (x == 1 or y == 1) and operation == '*':
+        msg += msg_7
+    if (x == 0 or y == 0) and operation in ['*', '-', '+']:
+        msg += msg_8
+    if msg != '':
+        msg = msg_9 + msg
+        print(msg)
+
 if __name__ == "__main__":
 
     while True:
         x, operation, y = take_input()
+        lazy_check(x, operation, y)
         result = make_calculations(x, operation, y)
         if result is False:
             continue
