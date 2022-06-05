bd8d72a89cda0a6abba6fe9d39c318c132bce250
danielSafonyan <daniel.safonyan@gmail.com>
2022-06-05 16:24:22 +0100

    Stage 3/5

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -2,6 +2,10 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+
+memory = 0.0
 
 
 def take_input():
@@ -11,10 +15,21 @@ def take_input():
         print(msg_0)
         x, operation, y = input().split()
 
-        try:
-            x = int(x)
-            y = int(y)
-        except ValueError:
+        if x == 'M':
+            try:
+                y = float(y)
+                x = memory
+            except ValueError:
+                print(msg_1)
+                continue
+        elif y == 'M':
+            try:
+                x = float(x)
+                y = memory
+            except ValueError:
+                print(msg_1)
+                continue
+        else:
             try:
                 x = float(x)
                 y = float(y)
@@ -43,14 +58,42 @@ def make_calculations(x, operation, y):
             return x / y
 
 
+def is_one_digit(num):
+    if num.is_integer():
+        num = int(num)
+        if -10 < num < 10:
+            return True
+    return False
+
+
 if __name__ == "__main__":
 
     while True:
         x, operation, y = take_input()
         result = make_calculations(x, operation, y)
-        if result is not False:
-            print(float(result))
+        if result is False:
+            continue
+        print(float(result))
+
+        while True:
+            print(msg_4)
+            answer = input()
+            if answer in ['y', 'n']:
+                break
+
+        if answer == 'y':
+            memory = result
+
+        while True:
+            print(msg_5)
+            answer = input()
+            if answer in ['y', 'n']:
+                break
+
+        if answer == 'n':
             break
+
+
 
 
 
